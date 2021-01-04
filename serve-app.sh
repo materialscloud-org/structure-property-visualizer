d933a469a1f29b5294d892e1ec858c5aeea3534b
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-04 13:14:21 +0100

    update dockerfile (#7)
    * update dockerfile to use environment variables
     * switch to github actions for testing

--- serve-app.sh
+++ serve-app.sh
@@ -1,24 +1,15 @@
 #!/bin/bash -e
-
-# This script is executed whenever the docker container is (re)started.
-
-#===============================================================================
-# debuging
 set -x
 
+# This script is executed whenever the docker container is (re)started.
 #===============================================================================
-## start postgres
-#source /opt/postgres.sh
-#psql_start
-
-#===============================================================================
-bokeh serve figure detail select-figure  \
+panel serve figure detail select-figure  \
     --port 5006                 \
     --log-level debug           \
     --allow-websocket-origin "*" \
     --prefix "$BOKEH_PREFIX" \
     --use-xheaders
-# --allow-websocket-origin discover.materialscloud.org 
+# --allow-websocket-origin discover.materialscloud.org
 # --allow-websocket-origin localhost:5006
 
 #===============================================================================
